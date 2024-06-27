be2f6be6d7b64053124d350e9e885e3ffdbbb001
Gia Hao <truonggiahao45@gmail.com>
2024-06-27 13:00:42 +0700

    reduce spaces in api.go

--- api.go
+++ api.go
@@ -5,40 +5,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
 	"github.com/gorilla/mux"
 )
 
-func WriteJSON(w http.ResponseWriter, status int, v any) error {
-	w.WriteHeader(status)
-	w.Header().Set("Content-Type", "application/json")
-	return json.NewEncoder(w).Encode(v)
-}
 
-type ApiError struct {
-	Error string
-}
-
-type apiFunc func(http.ResponseWriter, *http.Request) error
-
-func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if err := f(w, r); err != nil {
-			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
-		}
-
-	}
-}
-
+//Create a type struct APiServer
 type APIServer struct {
 	listenAddr string
 }
-
+//Initialize a new APIServer
 func newAPIServer(listenAddr string) *APIServer {
 	return &APIServer{
 		listenAddr: listenAddr,
 	}
 }
+
 func (s *APIServer) Run() {
 	router := mux.NewRouter()
 	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
@@ -58,6 +39,11 @@ func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error 
 		return fmt.Errorf("method not allowed %s", r.Method)
 	}
 }
+// An origin HandleFunc will not return an error
+// Ex: func handleGetAccount(w  http.ResponseWriter, r *http.Request){}
+// With this we'll have to handle the error inside of each HandleFunc and it will get messy. 
+// So in order to reduce the code we'll just handle the error inside of a single
+// makeHandleFunc function 
 func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
 	account := NewAccount("Hao", "Truong", "0828040144")
 	return WriteJSON(w, http.StatusCreated, account)
@@ -75,4 +61,24 @@ func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) 
 }
 func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
 	return nil
+}
+//Sub Func
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+type ApiError struct {
+	Error string
+}
+
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+		}
+	}
 }
